50fa501b8d2f80d52caa8729e52db6aa78325c2b
MohammadReza Vafazadeh <mrvafa@debian-BULLSEYE-live-builder-AMD64>
2023-06-27 08:43:27 +0330

    Initial commit

---
+++ frontend/src/app.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/messages')
+      .then(response => response.text())
+      .then(responseText => JSON.parse(responseText))
+      .then(data => setMessages(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {messages.map(message => (
+        <div key={message.id}>{message.text}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
